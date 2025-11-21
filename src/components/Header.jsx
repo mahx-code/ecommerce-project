489e217f75f7030af798c553ee0f7f31a3bc96ef
Mahmud <mahxgithub@gmail.com>
2025-11-21 21:22:13 +0100

    Added an extra functionality to the links in the nav to appear differently when they are in their own address or path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,34 @@
-import { Link } from "react-router";
-import './Header.css';
+import { NavLink } from "react-router";
+import "./Header.css";
+
 export default function Header() {
   return (
-    <div class="header">
-      <div class="left-section">
-        <Link to="/" class="header-link">
-          <img class="logo" src="images/logo-white.png" />
-          <img class="mobile-logo" src="images/mobile-logo-white.png" />
-        </Link>
+    <div className="header">
+      <div className="left-section">
+        <NavLink to="/" className="header-link">
+          <img className="logo" src="images/logo-white.png" />
+          <img className="mobile-logo" src="images/mobile-logo-white.png" />
+        </NavLink>
       </div>
 
-      <div class="middle-section">
-        <input class="search-bar" type="text" placeholder="Search" />
+      <div className="middle-section">
+        <input className="search-bar" type="text" placeholder="Search" />
 
-        <button class="search-button">
-          <img class="search-icon" src="images/icons/search-icon.png" />
+        <button className="search-button">
+          <img className="search-icon" src="images/icons/search-icon.png" />
         </button>
       </div>
 
-      <div class="right-section">
-        <Link class="orders-link header-link" to="/order">
-          <span class="orders-text">Orders</span>
-        </Link>
+      <div className="right-section">
+        <NavLink className="orders-link header-link" to="/order">
+          <span className="orders-text">Orders</span>
+        </NavLink>
 
-        <Link class="cart-link header-link" to="/checkout">
-          <img class="cart-icon" src="images/icons/cart-icon.png" />
-          <div class="cart-quantity">3</div>
-          <div class="cart-text">Cart</div>
-        </Link>
+        <NavLink className="cart-link header-link" to="/checkout">
+          <img className="cart-icon" src="images/icons/cart-icon.png" />
+          <div className="cart-quantity">3</div>
+          <div className="cart-text">Cart</div>
+        </NavLink>
       </div>
     </div>
   );

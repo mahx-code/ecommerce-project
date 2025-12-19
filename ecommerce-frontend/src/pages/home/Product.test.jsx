347f68a220e8cfbad93df0eb09d577136848991b
Mahmud <mahxgithub@gmail.com>
2025-12-19 13:10:23 +0100

    Use beforeEach to remove duplication

--- ecommerce-frontend/src/pages/home/Product.test.jsx
+++ ecommerce-frontend/src/pages/home/Product.test.jsx
@@ -1,10 +1,30 @@
-import { it, expect, describe, vi } from "vitest";
+import { it, expect, describe, vi, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Product from "./Product";
 import userEvent from "@testing-library/user-event";
 import axios from "axios";
 vi.mock("axios");
 describe("Product component", () => {
+  let product;
+
+  let loadCart;
+
+  beforeEach(() => {
+    product = {
+      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+      rating: {
+        stars: 4.5,
+        count: 87,
+      },
+      priceCents: 1090,
+      keywords: ["socks", "sports", "apparel"],
+    };
+
+    loadCart = vi.fn();
+  });
+
   it("displays the product details correctly", () => {
     const product = {
       id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -40,19 +60,6 @@ describe("Product component", () => {
   });
 
   it("adds a product to the cart", async () => {
-    const product = {
-      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-      rating: {
-        stars: 4.5,
-        count: 87,
-      },
-      priceCents: 1090,
-      keywords: ["socks", "sports", "apparel"],
-    };
-
-    const loadCart = vi.fn();
     render(<Product product={product} loadCart={loadCart} />);
 
     const addToCartButton = screen.getByTestId("add-to-cart-button");

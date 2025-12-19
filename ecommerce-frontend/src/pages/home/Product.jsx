525b82c80b5d9a476cc0d6c355bb3f667bf313c2
Mahmud <mahxgithub@gmail.com>
2025-12-19 13:02:31 +0100

    Test click to add button

--- ecommerce-frontend/src/pages/home/Product.jsx
+++ ecommerce-frontend/src/pages/home/Product.jsx
@@ -66,7 +66,7 @@ export default function Product({ product, loadCart }) {
         Added
       </div>
 
-      <button className="add-to-cart-button button-primary" onClick={addToCart}>
+      <button data-testid="add-to-cart-button" className="add-to-cart-button button-primary" onClick={addToCart}>
         Add to Cart
       </button>
     </div>
